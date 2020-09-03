c65f00ae53a1687f3125980225e3319a2cb1beed
Master <1204332316@qq.com>
2020-09-03 17:52:01 +0800

    add

---
+++ my-web-maven/src/main/java/com/master/HelloWord.java
@@ -0,0 +1,12 @@
+package com.master;
+
+/**
+ * <p>Description:</p>
+ * @author xiajinhui
+ * @Time 2020年9月3日下午5:50:55
+ */
+public class HelloWord {
+	public static void main(String[] args) {
+		System.out.println("hello-world");
+	}
+}
